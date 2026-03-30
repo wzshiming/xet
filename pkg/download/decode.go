670566a84bf9d987bd11db7bc2ce98e266468b52
Shiming Zhang <wzshiming@hotmail.com>
2026-03-30 15:38:30 +0800

    Clean up

--- pkg/download/decode.go
+++ pkg/download/decode.go
@@ -2,11 +2,12 @@ package download
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/wzshiming/xet/pkg/xorb"
 )
 
 // ClientAdapter provides access to client operations needed for reconstruction decoding
 type ClientAdapter interface {
-	DownloadXorb(ctx context.Context, url string, reqOpts ...interface{}) (*xorb.Xorb, error)
+	DownloadXorb(ctx context.Context, url string, header http.Header) (*xorb.Xorb, error)
 }
