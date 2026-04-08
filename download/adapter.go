fb9a85a3a3abe33890068e75991d63275e5492e8
Shiming Zhang <wzshiming@hotmail.com>
2026-04-08 17:07:20 +0800

    Clean up

--- download/adapter.go
+++ download/adapter.go
@@ -2,6 +2,9 @@ package download
 
 import (
 	"context"
+	"io"
+	"mime/multipart"
+	"net/http"
 
 	"github.com/wzshiming/xet"
 )
@@ -10,4 +13,10 @@ import (
 type StorageAdapter interface {
 	GetXorbURL(namespace string, xorbHash xet.Hash) string
 	GetXorbDataRange(ctx context.Context, namespace string, xorbHash xet.Hash, chunkStart, chunkEnd uint32) (startByte, endByte int64, err error)
+}
+
+// ClientAdapter provides access to client operations needed for reconstruction decoding
+type ClientAdapter interface {
+	DownloadXorb(ctx context.Context, url string, header http.Header) (io.ReadCloser, error)
+	DownloadXorbsMultipart(ctx context.Context, url string, header http.Header) (*multipart.Reader, io.Closer, error)
 }
--- download/download.go
+++ download/download.go
@@ -1,19 +1,5 @@
 package download
 
-import (
-	"context"
-	"io"
-	"mime/multipart"
-	"net/http"
-)
-
-// ClientAdapter provides access to client operations needed for reconstruction decoding
-type ClientAdapter interface {
-	DownloadXorb(ctx context.Context, url string, header http.Header) (io.ReadCloser, error)
-
-	DownloadXorbsMultipart(ctx context.Context, url string, header http.Header) (*multipart.Reader, io.Closer, error)
-}
-
 type options struct {
 	concurrency int
 	retries     int
